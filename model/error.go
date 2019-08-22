35ed60cad891017a724053e003baf86358509f51
dsw <danangsukma@gmail.com>
2019-08-22 22:26:30 +0700

    Add models post and error

---
+++ model/error.go
@@ -0,0 +1,8 @@
+package model
+
+import "errors"
+
+// ErrNotFound error message
+var (
+	ErrNotFound = errors.New("error not found")
+)
---
+++ model/post.go
@@ -0,0 +1,10 @@
+package model
+
+// Post type details
+type Post struct {
+	ID      int64  `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	// created_at time.Time `json:"created_at"`
+	// updated_at time.Time `json:"updated_at"`
+}
